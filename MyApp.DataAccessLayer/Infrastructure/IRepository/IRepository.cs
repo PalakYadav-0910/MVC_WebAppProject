662ca3b89bc89f0fc5a2e656897ddc08dfdb6be9
PalakYadav-0910 <sy8220718@gmail.com>
2025-08-10 20:40:57 +0530

    Implemented Repository Pattern

---
+++ MyApp.DataAccessLayer/Infrastructure/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace MyApp.DataAccessLayer.Infrastructure.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetT(Expression<Func<T, bool>> predicate);
+        void Add(T entity);
+        void Delete(T entity);
+        void DeleteRange(IEnumerable<T> entities);
+
+    }
+}
---
+++ MyApp.DataAccessLayer/Infrastructure/Repository/Repository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.DataAccessLayer.Data;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using System.Linq.Expressions;
+
+namespace MyApp.DataAccessLayer.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        // Assuming you have a DbContext injected here
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+        public T GetT(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate).FirstOrDefault();
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _dbSet.RemoveRange(entities);
+        }
+    }
+}
